1512a797b8fee5c7535a70d0a1ba0cc3e936c2a2
anubhav0303 <anubhav143844@gmail.com>
2024-11-16 00:03:41 +0530

    1st commit
    working

--- po.js
+++ po.js
@@ -1,1 +1,18 @@
 
+let imgBox = document.getElementById("imgBox");
+let QRimg = document.getElementById("QRimg");
+let qrtxt = document.getElementById("qrtxt");
+
+function generateQR() {
+    if (qrtxt.value.length > 0) {
+        QRimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrtxt.value);
+        imgBox.classList.add("show-img");
+    } 
+    else {
+        // alert("Please Enter the Value");
+        qrtxt.classList.add("error");
+    setTimeout(function () {
+      qrtxt.classList.remove("error");
+    }, 500);
+}
+}
